f88e8612cabf5abaced966b7e9308bf88c6f3934
Justin Skists <justin.skists@juzza.co.uk>
2022-11-15 10:47:37 +0000

    initial rsx experiments
    start development

---
+++ apps/fnrsx/main.asm
@@ -0,0 +1,17 @@
+; compile with  z88dk-z80asm -v -b -reloc-info -l -s -m -g -omain.bin main.asm
+
+jumptable:
+    jp REL_init
+    jp REL_test
+
+
+REL_init:
+    ld hl, 0x1234
+    ret
+
+REL_test:
+    call REL_subroutine
+    ret
+
+REL_subroutine:
+    ret
