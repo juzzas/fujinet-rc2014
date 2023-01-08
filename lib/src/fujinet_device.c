00e19a528d5f8d372a8d2ddad40a6e90e112d292
Justin Skists <justin.skists@juzza.co.uk>
2023-01-08 11:23:54 +0000

    fix directory opening

--- lib/src/fujinet_device.c
+++ lib/src/fujinet_device.c
@@ -164,16 +164,16 @@ FUJINET_RC fujinet_mount_host_slot(unsigned char hs)
     return fujinet_dcb_exec(&dcb);
 }
 
+char open_dir_buffer[258];
 FUJINET_RC fujinet_open_directory(unsigned char hs, char *p, char *f)
 {
     memset(&dcb, 0, sizeof(struct fujinet_dcb));
-    char c[258];
     char *e;
 
-    memset(&c,0,258);
+    memset(&open_dir_buffer,0,258);
 
-    strcpy(c, p);
-    e = c;
+    strcpy(open_dir_buffer, p);
+    e = open_dir_buffer;
 
     if (f[0]!=0x00) {
         while (*e != 0x00)
@@ -185,7 +185,7 @@ FUJINET_RC fujinet_open_directory(unsigned char hs, char *p, char *f)
 
     dcb.device = 0x70;
     dcb.command = 0xF7;
-    dcb.buffer = (uint8_t *)c;
+    dcb.buffer = (uint8_t *)open_dir_buffer;
     dcb.buffer_bytes = 256;
     dcb.timeout = 15;
     dcb.aux1 = hs;
@@ -237,7 +237,7 @@ FUJINET_RC fujinet_set_device_filename(uint8_t ds, char* e)
     memset(&dcb, 0, sizeof(struct fujinet_dcb));
 
     dcb.device = 0x70;
-    dcb.command = 0xE4;
+    dcb.command = 0xE2;
     dcb.timeout = 15;
     dcb.buffer = (uint8_t *)e;
     dcb.buffer_bytes = 256;
