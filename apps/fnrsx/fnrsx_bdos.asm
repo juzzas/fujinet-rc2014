23e094e822d5741970d00f1cdfb58c7d40700188
Justin Skists <justin.skists@juzza.co.uk>
2023-02-01 11:32:53 +0000

    bdos handler

--- apps/fnrsx/fnrsx_bdos.asm
+++ apps/fnrsx/fnrsx_bdos.asm
@@ -2,11 +2,27 @@
 
 PUBLIC bdos_handler
 PUBLIC old_bdos
+extern fujinet_dcb_exec
 
 old_bdos:
     DEFS 3
 
 
 bdos_handler:
+    ld a, c
+    cp 0xf0
+    jr z, handle_bdos_fujinet
     jp old_bdos
+
+; entry: de = fuginet_dcb
+; exit:   a = FUJINET_RC
+handle_bdos_fujinet:
+    push hl
+    ld hl, de
+    call fujinet_dcb_exec
+    pop hl
+    ret
+
+
+
 
