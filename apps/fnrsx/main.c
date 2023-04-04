e1b9101be51b842ccb9dcdc63858a6e6484a2c79
Justin Skists <justin.skists@juzza.co.uk>
2023-04-04 09:49:46 +0100

    start implementation of fnrsx

--- apps/fnrsx/main.c
+++ apps/fnrsx/main.c
@@ -1,54 +1,39 @@
-//
-// Created by jskists on 19/12/2022.
-//
+/*
+ * Copyright (c) 2023 Justin Skists.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 
 #include <stdio.h>
 #include <stdint.h>
 #include <string.h>
+#include <arch/cpm.h>
 #include <z80.h>
 
-#define DEBUG 1
-
-#pragma output REGISTER_SP = 32768
-
-/*
- * Exported values from loader.asm
- */
-extern const uint8_t* driver_code;
-extern const uint16_t driver_code_size;
-extern const uint16_t* reloc_info;
-extern const uint16_t reloc_info_size;
-
-extern void driver_code_init(uint16_t init_call_addr) __z88dk_fastcall;
-
-uint16_t load_addr;
+#define DEBUG
 
 void main(void) {
-  puts("FujNet RSX loader");
-
-  uint16_t bdos_addr = z80_wpeek(0x0006);
-  uint16_t driver_pages = ((driver_code_size / 256) + 1);
-
-  load_addr = (bdos_addr - (driver_pages << 8)) & 0xff00;
-
-#if DEBUG
-  printf("driver code = 0x%04x (%x bytes)\n", driver_code, driver_code_size);
-  printf("reloc info = 0x%04x (%u words)\n", reloc_info, reloc_info_size);
-  printf("bdos = 0x%04x\n", bdos_addr);
-  printf("pages needed = %u\n", driver_pages);
-  printf("load_addr = 0x%04x\n", load_addr);
+#ifdef DEBUG
+    printf("new bdos = 0x%04x\n", z80_wpeek(0x0006));
 #endif
 
-  memcpy((uint8_t*)load_addr, driver_code, driver_code_size);
+    int rc = cpm_bdos(0xf0, 0); // fujinet_init
 
-  for (int i = 0; i < reloc_info_size; i++) {
-    uint16_t addr = z80_wpeek((load_addr + reloc_info[i]));
-    printf("patching 0x%04x: 0x%04x > 0x%04x\n", reloc_info[i], addr, addr + load_addr);
-    z80_wpoke(load_addr + reloc_info[i], addr + load_addr);
-  }
-
-  driver_code_init(load_addr);
-
-  puts("FujNet RSX installed");
-  printf("new bdos = 0x%04x\n", z80_wpeek(0x0006));
+    puts("FujiNet RSX installed");
 }
