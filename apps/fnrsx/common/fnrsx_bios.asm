55fb96bb25d315d99e3946282cb28f9f241ec116
Justin Skists <justin.skists@juzza.co.uk>
2023-07-15 12:55:16 +0100

    fnrsx: bios: clea up tabs

--- apps/fnrsx/common/fnrsx_bios.asm
+++ apps/fnrsx/common/fnrsx_bios.asm
@@ -56,80 +56,81 @@ patch_bdos:
 
 
 patch_bios:
-	; Now copy jumpblock and patch it for our use
-	ld hl,(1)	;; this is the address of the JP for wboot
-	or a
-	ld de,3
-	sbc hl,de
-	;; HL = JP for boot
-	push hl
-	ld de,jumpblock_copy
-	ld bc,cpm_bdos_jumpblock_size
-	ldir
-	pop hl
+    ; Now copy jumpblock and patch it for our use
+    ld hl,(1)	;; this is the address of the JP for wboot
+    or a
+    ld de,3
+    sbc hl,de
+    ;; HL = JP for boot
+    push hl
+    ld de,jumpblock_copy
+    ld bc,cpm_bdos_jumpblock_size
+    ldir
+    pop hl
 
-	;; now patch
-	;ld bc,cpm_boot
-	;ld de,patched_boot
-	;call patch
+    ;; now patch
+    ;ld bc,cpm_boot
+    ;ld de,patched_boot
+    ;call patch
 
-	;ld bc,cpm_wboot
-	;ld de,patched_wboot
-	;call patch
+    ;ld bc,cpm_wboot
+    ;ld de,patched_wboot
+    ;call patch
 
-	ld bc,cpm_conout
-	ld de,patched_conout
-	call patch
+    ld bc,cpm_conout
+    ld de,patched_conout
+    call patch
 
-	ld bc,cpm_list
-	ld de,patched_list
-	call patch
+    ld bc,cpm_list
+    ld de,patched_list
+    call patch
 
-;	ld bc,cpm_home
-;	ld de,patched_home
-;	call patch
 
-;	ld bc,cpm_seldsk
-;	ld de,patched_seldsk
-;	call patch
+;    ld bc,cpm_home
+;    ld de,patched_home
+;    call patch
 
-;	ld bc,cpm_settrk
-;	ld de,patched_settrk
-;	call patch
+;    ld bc,cpm_seldsk
+;    ld de,patched_seldsk
+;    call patch
 
-;	ld bc,cpm_setsec
-;	ld de,patched_setsec
-;	call patch
+;    ld bc,cpm_settrk
+;    ld de,patched_settrk
+;    call patch
 
-;	ld bc,cpm_setdma
-;	ld de,patched_setdma
-;	call patch
+;    ld bc,cpm_setsec
+;    ld de,patched_setsec
+;    call patch
 
-;	ld bc,cpm_read
-;	ld de,patched_read
-;	call patch
+;    ld bc,cpm_setdma
+;    ld de,patched_setdma
+;    call patch
+
+;    ld bc,cpm_read
+;    ld de,patched_read
+;    call patch
 ;
-;	ld bc,cpm_write
-;	ld de,patched_write
-;	call patch
+;    ld bc,cpm_write
+;    ld de,patched_write
+;    call patch
 ;
-;	ld bc,cpm_sectran
-;	ld de,patched_sectran
-;	call patch
-	ret
+;    ld bc,cpm_sectran
+;    ld de,patched_sectran
+;    call patch
+    ret
 
 ;;------------------------------------------------------------------------------------------------------------
 patch:
-	push hl
-	add hl,bc
-	ld a,0xc3
-	ld (hl),a
-	inc hl
-	ld (hl),e
-	inc hl
-	ld (hl),d
-	pop hl
-	ret
+    push hl
+    add hl,bc
+    ld a,0xc3
+    ld (hl),a
+    inc hl
+    ld (hl),e
+    inc hl
+    ld (hl),d
+    pop hl
+    ret
 
 
 ;;------------------------------------------------------------------------------------------------------------
@@ -139,13 +140,13 @@ patch:
 ;;This function is completely implementation-dependent and should never be called from user code.
 
 patched_boot:
-	push hl
-	push af
-	;ld hl,log_boot
-	;call display_message
-	pop af
-	pop hl
-	jp jumpblock_copy+cpm_boot
+    push hl
+    push af
+    ;ld hl,log_boot
+    ;call display_message
+    pop af
+    pop hl
+    jp jumpblock_copy+cpm_boot
 
 
 ;;------------------------------------------------------------------------------------------------------------
@@ -155,12 +156,12 @@ patched_boot:
 ;;Reloads the command processor and (on some systems) the BDOS as well. How it does this is implementation-dependent; it may use the reserved tracks of a floppy disc or extra memory.
 ;; Probably only needed for CP/M 2.2
 patched_wboot:
-	;; do normal wboot
-	call jumpblock_copy+cpm_wboot
-	;; repatch jump block
-	call patch_bdos
-	call patch_bios
-	ret
+    ;; do normal wboot
+    call jumpblock_copy+cpm_wboot
+    ;; repatch jump block
+    call patch_bdos
+    call patch_bios
+    ret
 
 ;;------------------------------------------------------------------------------------------------------------
 ;; From seasip.info:
